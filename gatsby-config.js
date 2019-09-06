712eb0b8a464dd069dd8439646b5d4951bde69fd
sensoph1 <sensoph@gmail.com>
2019-09-06 15:50:06 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `JeremyBarkley.com`,
+    author: `Jeremy Barkley`,
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `unknown.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `zombiestiltskin`,
     },
   },
   plugins: [
